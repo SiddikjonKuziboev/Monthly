f89352b52bbebeff3f77008fcb851a3132e3dd33
Siddiqjon <kuziboevsiddiqjon98@gmail.com>
2023-09-11 14:27:13 +0500

    Initial Commit

---
+++ Monthly/MonthlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonthlyApp.swift
+//  Monthly
+//
+//  Created by Kuziboev Siddikjon on 11/09/23.
+//
+
+import SwiftUI
+
+@main
+struct MonthlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
